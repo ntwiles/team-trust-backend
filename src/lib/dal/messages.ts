6fa703b6a64d5a73bab27d007a6b9561c753031d
Nathan Wiles <ntwiles@gmail.com>
2021-04-24 01:55:30 -0400

    Add dummy data.

--- src/lib/dal/messages.ts
+++ src/lib/dal/messages.ts
@@ -2,16 +2,38 @@ import { IChannel } from '../../types/channel'
 import { IMessage } from '../../types/message'
 
 const messages: IMessage[] = [
-  {
-    body: 'Hey, this is our first message!',
-    channel: {
-      interest: 'programming',
-      location: 'cincinnati',
+    {
+        body: 'Hey, I have an idea. We should do a local hackathon.',
+        channel: {
+            interest: 'programming',
+            location: 'cincinnati',
+        },
+        timestamp: new Date(),
+        user: '3a374bf1-a411-43a7-b9d4-77fb2a068aca',
     },
-    timestamp: new Date(),
-    user: '3a374bf1-a411-43a7-b9d4-77fb2a068aca',
-  },
+    {
+        body: "That's a great idea!",
+        channel: {
+            interest: 'programming',
+            location: 'cincinnati',
+        },
+        timestamp: new Date(),
+        user: '2d402411-3773-4a73-81ed-702316832cb0',
+    },
+    {
+        body: 'We can meet up to present our projects.',
+        channel: {
+            interest: 'programming',
+            location: 'cincinnati',
+        },
+        timestamp: new Date(),
+        user: 'c4b25ebe-a41a-431b-9265-3d2e21d4dc04',
+    },
 ]
 
 export const getMessagesByChannel = (channel: IChannel) =>
-  messages.filter(m => m.channel === channel)
+    messages.filter(
+        (m) =>
+            m.channel.interest === channel.interest &&
+            m.channel.location === channel.location
+    )
--- src/lib/dal/users.ts
+++ src/lib/dal/users.ts
@@ -2,33 +2,63 @@ import { IChannel } from '../../types/channel'
 import { IUser } from '../../types/user'
 
 const users: IUser[] = [
-  {
-    id: '3a374bf1-a411-43a7-b9d4-77fb2a068aca',
-    displayName: 'Nathan Wiles',
-    interests: ['programming', 'reading'],
-    locations: ['cincinnati'],
-  },
-  {
-    id: 'd8719507-9ae3-4278-9531-6d385150eb4b',
-    displayName: 'Colin Feris',
-    interests: ['programming', 'reading'],
-    locations: ['cincinnati'],
-  },
-  {
-    id: '7944d338-4299-484e-b368-fcf9de2860a7',
-    displayName: 'Bhawana Yadav',
-    interests: ['programming', 'reading'],
-    locations: ['cincinnati'],
-  },
+    {
+        id: '3a374bf1-a411-43a7-b9d4-77fb2a068aca',
+        displayName: 'Walter White',
+        interests: ['programming', 'reading', 'gaming'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: 'd8719507-9ae3-4278-9531-6d385150eb4b',
+        displayName: 'Skylar White',
+        interests: ['hiking', 'reading', 'travel'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: '68a9a62a-2200-4cbf-8013-16cd855203da',
+        displayName: 'Jesse Pinkman',
+        interests: ['programming', 'reading', 'cooking'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: '6462bebf-1450-49f1-bb18-d3b145f02857',
+        displayName: 'Michael Ehrmantraut',
+        interests: ['programming', 'cooking', 'travel'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: 'c4b25ebe-a41a-431b-9265-3d2e21d4dc04',
+        displayName: 'Gustavo Fring',
+        interests: ['gaming', 'hiking', 'reading'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: '26a15a2b-837a-478d-be07-8d3d865c6ed3',
+        displayName: 'James McGill',
+        interests: ['cooking', 'travel', 'reading'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: '2d402411-3773-4a73-81ed-702316832cb0',
+        displayName: 'Kimberly Wexler',
+        interests: ['hiking', 'reading', 'gaming'],
+        locations: ['cincinnati'],
+    },
+    {
+        id: 'ebb27b7d-4137-4d7b-b8bd-e8a1cd2bf038',
+        displayName: 'Lydia Rodarte-Quayle',
+        interests: ['gaming', 'reading', 'cooking'],
+        locations: ['cincinnati'],
+    },
 ]
 
 export const getUserById = (userId: string): IUser =>
-  users.find(u => u.id === userId)
+    users.find((u) => u.id === userId)
 
 export const getUsersByChannel = (channel: IChannel): IUser[] => {
-  return users.filter(
-    u =>
-      u.locations.includes(channel.location) &&
-      u.interests.includes(channel.interest),
-  )
+    return users.filter(
+        (u) =>
+            u.locations.includes(channel.location) &&
+            u.interests.includes(channel.interest)
+    )
 }
