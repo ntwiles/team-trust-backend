e5edd73c99f52dc0bb4f606fcfd94adfe97b8ff9
Nathan Wiles <ntwiles@gmail.com>
2021-04-26 12:42:23 -0400

    Add cors.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 /* eslint-disable @typescript-eslint/unbound-method, @typescript-eslint/no-misused-promises */
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { InterestsController } from './controllers/interests'
 import { LocationsController } from './controllers/locations'
 import { MessagesController } from './controllers/messages'
 import { UsersController } from './controllers/users'
 import { init } from './middleware/init'
 import { HttpError } from './types/error'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(init)
 
 app.get('/user/:userId', UsersController.show)
@@ -23,9 +25,9 @@ app.post('/messages/:location/:interest', MessagesController.create)
 app.get('/locations', LocationsController.index)
 app.get('/interests', InterestsController.index)
 
-app.use((err: HttpError, req: Request, res: Response) => {
-    console.log(err)
-    res.status(err.status).send(err.message)
+app.use((err: HttpError, req: Request, res: Response, _: NextFunction) => {
+    console.log(err.message)
+    res.status(err.status).send()
 })
 
 app.listen(port, () => console.log('Listening @ http://localhost:3000'))
